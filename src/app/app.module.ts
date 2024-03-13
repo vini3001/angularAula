3891f0522fd55c3651aa85e1ed43ffefc05058c7
Vinícius Donizeti <donizetevinicius250@gmail.com>
2024-03-12 21:19:07 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimeiroComponenteComponent } from './primeiro-componente/primeiro-componente.component';
+import { FormsModule } from '@angular/forms';
+import { SegundoComponenteComponent } from './segundo-componente/segundo-componente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimeiroComponenteComponent,
+    SegundoComponenteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/primeiro-componente/primeiro-componente.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-primeiro-componente',
+  templateUrl: './primeiro-componente.component.html',
+  styleUrl: './primeiro-componente.component.css'
+})
+export class PrimeiroComponenteComponent {
+
+  public inputValue = ""  
+  public minhaCor = "Red"
+}
